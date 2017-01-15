c3072d55295633be1f37c7a4ef0d2b5458f3144e
Travis Roman <ffx1033@gmail.com>
2017-01-15 18:58:50 -0500

    No longer use absolute path in compiler

--- js/injected/compile.sh
+++ js/injected/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-MAIN_DIR="/Users/Travis/Documents/Extensions/JS Blocker/JavaScript Blocker 5.git.safariextension"
-# BETA_DIR="/Users/Travis/Documents/Extensions/JS Blocker/JavaScript Blocker 5.BETA.safariextension"
+
+MAIN_DIR="$(dirname "$(dirname "$PWD")")"
+
 cd "$MAIN_DIR/js/"
 
 echo -n > injected/compiled.js
@@ -9,10 +10,6 @@ echo "if (!window.JSB_INJECTED) { window.JSB_INJECTED = true;" > injected/compil
 echo >> injected/compiled.js
 
 /bin/cat safari.js promise.js utilities.js event.js store.js injected/commands.js injected/blocker.js injected/deepInject.js injected/notification.js injected/special.js injected/specials.js injected/userScript.js >> injected/compiled.js
-
-# rm "$BETA_DIR/js/injected/compiled.js"
-
-# /Users/Travis/bin/link_directory.sh "$MAIN_DIR" "$BETA_DIR"
 
 echo "} else { console.warn('Attempt to inject JSB more than once blocked.'); }" >> injected/compiled.js
 
